3f7872918849380f89bdc581a1cc12b27d8af725
renselange <renselange@gmail.com>
2013-05-03 18:34:55 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+
+# python setup.py sdist
+#
+
+from distutils.core import setup
+
+setup(name ='webcat',author = 'Rense Lange', author_email = 'renselange@earthlink.net', version = '1.0',scripts = ['webcat.py'],packages = ['defshere'])
